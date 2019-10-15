6a0a781e03e8a87dcbb3818cb5624839e1df1cbc
TheAeryan <carlosmarista@gmail.com>
2019-10-15 18:23:23 +0200

    Finished MultiTouch project

--- app/src/main/java/com/example/marcelinocabrera/multitouch/MultiTouchView.java
+++ app/src/main/java/com/example/marcelinocabrera/multitouch/MultiTouchView.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.PointF;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.util.Log;
@@ -22,9 +25,14 @@ public class MultiTouchView extends View {
     private static final int STROKE_WIDTH = 10;
     private static final int CIRCLE_RADIUS = 30;
 
-    // Lista de puntos pulsados en la pantalla
-    private ArrayList<PointF> touchPoints = null;
+    private PointF point = null;
     private Paint drawingPaint = null;
+
+    private float minDistTouch = 100; // Distancia mínima en píxeles a la que tiene que estar el puntero para que se pueda mover el punto
+                                      // Solo se aplica a la hora de tocar la pantalla para empezar a mover el punto
+
+    private boolean dragStarted = false;
+
 
     public MultiTouchView(Context context) {
         super(context);
@@ -41,46 +49,17 @@ public class MultiTouchView extends View {
         initialize();
     }
 
-    // Muestra los puntos en la pantalla (incluidos la linea y el punto medio)
+    // Dibuja el punto
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if(touchPoints.size() > 0)
-        {
-            if(touchPoints.size() == 1) // Muesta un punto en pantalla
-            {
-                canvas.drawCircle(touchPoints.get(0).x,touchPoints.get(0).y,CIRCLE_RADIUS, drawingPaint);
-            } else // Muestra un los puntos unidos con una linea y el punto medio
-            {
-                PointF midpt = null;
-                for(int index=1; index<touchPoints.size(); ++index)
-                {
-                    midpt = getMidPoint(
-                            touchPoints.get(index - 1).x,touchPoints.get(index - 1).y,
-                            touchPoints.get(index).x,touchPoints.get(index).y);
-                    canvas.drawCircle(touchPoints.get(index - 1).x,touchPoints.get(index - 1).y,CIRCLE_RADIUS, drawingPaint);
-                    canvas.drawCircle(touchPoints.get(index).x,touchPoints.get(index).y,CIRCLE_RADIUS, drawingPaint);
-                    canvas.drawLine(
-                            touchPoints.get(index - 1).x,touchPoints.get(index - 1).y,
-                            touchPoints.get(index).x,touchPoints.get(index).y,drawingPaint);
-                    canvas.drawCircle(midpt.x,midpt.y, CIRCLE_RADIUS/2, drawingPaint);
-                }
-            }
-            // Redibuja el canvas
+        if(point != null){
+            canvas.drawCircle(point.x,point.y,CIRCLE_RADIUS, drawingPaint);
             invalidate();
         }
-
     }
 
-    // Fija los puntos a dibujar
-    public void setPoints(MotionEvent event){
-        touchPoints.clear(); // Elimina la lista anterior
-        for(int index=0; index<event.getPointerCount(); ++index)
-        {
-            // Obtiene la lista de puntos pulsados del MotionEvent
-            touchPoints.add(new PointF(event.getX(index),event.getY(index)));
-        }
-    }
+
 
     // METODO IMPORTANTE
     // Control de la multipulsacion de la pantalla
@@ -94,31 +73,22 @@ public class MultiTouchView extends View {
         switch(action) {
             // Pulsamos
             case MotionEvent.ACTION_DOWN: {
-                // Veo si hay algún punto "cercano"
-                float min_dist = 100;
-                PointF pos_pulsacion = new PointF(event.getX(), event.getY());
-                float dist;
-                boolean ya_encontrado = false;
+                PointF pointer = new PointF(event.getX(), event.getY());
 
-                for(int i=0; i<touchPoints.size() && !ya_encontrado; i++){
-                    // Calculo la distancia entre ese punto y el nuevo
-                    dist = getDistance(pos_pulsacion, touchPoints.get(i));
+                // Compruebo que el punto esté dentro de la vista
+                if(pointer.x < getWidth() && pointer.y < getHeight()){
+                    float point_dist = getDistance(point, pointer);
 
-                    // He pulsado sobre un punto ya existente
-                    if (dist <= min_dist){
-                        // Cambio la posición del punto
-                        touchPoints.get(i).x = pos_pulsacion.x;
-                        touchPoints.get(i).y = pos_pulsacion.y;
-                        ya_encontrado = true;
+                    if (point_dist < minDistTouch) { // Compruebo si he pulsado cerca del punto
+                        point.x = pointer.x;
+                        point.y = pointer.y;
+
+                        dragStarted = true;
                     }
+
+                    invalidate(); // Redibuja
                 }
 
-                // No existe ningún punto en esa posición -> creo uno nuevo
-                if (!ya_encontrado){
-                    touchPoints.add(pos_pulsacion);
-                }
-
-                invalidate(); // Redibuja
                 Log.i("INFO", "Presión:" + event.getPressure());
                 Log.i("INFO", "Tamaño:" + event.getSize());
 
@@ -126,31 +96,20 @@ public class MultiTouchView extends View {
             }
             // Movemos
             case MotionEvent.ACTION_MOVE:   {
-                // Veo si hay algún punto "cercano"
-                float min_dist = 75;
-                PointF pos_pulsacion = new PointF(event.getX(), event.getY());
-                float dist;
-                boolean ya_encontrado = false;
+                PointF pointer = new PointF(event.getX(), event.getY());
 
-                for(int i=0; i<touchPoints.size() && !ya_encontrado; i++){
-                    // Calculo la distancia entre ese punto y el nuevo
-                    dist = getDistance(pos_pulsacion, touchPoints.get(i));
-
-                    // He pulsado sobre un punto ya existente
-                    if (dist <= min_dist){
-                        // Cambio la posición del punto
-                        touchPoints.get(i).x = pos_pulsacion.x;
-                        touchPoints.get(i).y = pos_pulsacion.y;
-                        ya_encontrado = true;
+                if (dragStarted) {
+                    // Compruebo que el punto esté dentro de la vista
+                    if (pointer.x < getWidth() && pointer.y < getHeight()) {
+                        point.x = pointer.x;
+                        point.y = pointer.y;
                     }
+                    else{ // Si está fuera, vuelve a su posición inicial
+                        initialize();
+                    }
+                    invalidate(); // Redibuja
                 }
 
-                // No existe ningún punto en esa posición -> creo uno nuevo
-                if (!ya_encontrado){
-                    touchPoints.add(pos_pulsacion);
-                }
-
-                invalidate(); // Redibuja
                 Log.i("INFO", "Presión:" + event.getPressure());
                 Log.i("INFO", "Tamaño:" + event.getSize());
 
@@ -158,52 +117,41 @@ public class MultiTouchView extends View {
             }
             // Levantamos
             case MotionEvent.ACTION_UP:   {
-                // initialize (); // Borra la pantalla
+                dragStarted = false;
+
                 break;
             }
 
             // Pulsamos con mas de un dedo
             case MotionEvent.ACTION_POINTER_DOWN: {
-                setPoints(event); //
-                invalidate();
-                if (touchPoints.size() == 5) Log.i ("INFO","Cinco dedos");
-                if (touchPoints.size() == 10) Log.i ("INFO","Diez dedos");
                 break;
             }
             // Levantamos
             case MotionEvent.ACTION_POINTER_UP:   {
-                // initialize (); // Borra la pantalla
                 break;
             }
 
             // La ventana pierde el focus
             case MotionEvent.ACTION_CANCEL:{
-                initialize(); // Borra la pantalla
+                initialize(); // El punto vuelve a su posición original
                 break;
             }
-
         }
 
 
         return true;
     }
+
+
     private void initialize(){
         drawingPaint = new Paint();
         drawingPaint.setColor(Color.MAGENTA);
         drawingPaint.setStrokeWidth(STROKE_WIDTH);
         drawingPaint.setStyle(Paint.Style.FILL);
-        drawingPaint.setAntiAlias(true);{
+        drawingPaint.setAntiAlias(true);
 
-        }
-        touchPoints = new ArrayList<PointF>();
-    }
-
-    private PointF getMidPoint(float x1,float y1, float x2, float y2) {
-        PointF point = new PointF();
-        float x = x1 + x2;
-        float y = y1 + y2;
-        point.set(x / 2, y / 2);
-        return point;
+        // Inicialmente el punto está en el medio de la pantalla
+        point = new PointF(500, 700);
     }
 
     private float getDistance(PointF a, PointF b){
@@ -213,4 +161,6 @@ public class MultiTouchView extends View {
 
         return (float)distance;
     }
+
+
 }
